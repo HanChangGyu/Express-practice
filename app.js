717a5a1bc6cf2174ddd4a453b3dd2907e843ea7f
HanChangGyu <ceu0723@gmail.com>
2025-10-30 11:59:58 +0900

    feat: 에러 핸들링 미들웨어 추가

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const path = require('path');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+
+app.use((req, res, next) => {
+    console.log('모든 요청에 실행하고 싶어요');
+    // res.send('안녕하세요'); - 에러난다
+    next();
+}, (req, res, next) => {
+    throw new Error('에러야'); // 에러를 발생시키는 일반 미들웨어
+})
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -12,10 +20,26 @@ app.post('/', (req, res) => {
     res.send('hello express!');
 });
 
+app.get('/category/:name', (req, res) => {
+    res.send(`hello wildcard`);
+});
+
+app.get('/category/Javascript', (req, res) => {
+    res.send(`hello Javascript`);
+});
+
 app.get('/about', (req, res) => {
     res.send('hello express');
 });
 
+app.use((req, res, next) => { // 일반 미들웨어 - 라우터가 다 무시되고 여기까지 오면 404
+    res.status(200).send('404지롱');
+});
+
+app.use((err, req, res, next) => { // 에러 처리 전용 미들웨어 'throw new Error(...)' or 'next(err)' 발생하면
+    console.error(err);
+    res.status(200).send('에러났어용') 
+})
 app.listen(app.get('port'), () => {
     console.log('익스프레스 서버 실행');
 });
