cff51a49acb70526dfe88713f2e51cb842cd100c
HanChangGyu <ceu0723@gmail.com>
2025-11-03 18:08:57 +0900

    feat: express-session 기능 및 설명추가

--- app.js
+++ app.js
@@ -3,20 +3,36 @@ const path = require('path');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const multer = reqiuire('multer'); // 파일이나 그림을 받으면 저장할 수 있음
+const multer = reqiuire('multer'); 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
-app.use('/', express.static(__dirname, 'public' )); // 실제 파일이 존재하면 다음 미들웨어로 넘기지 않음
 app.use(cookieParser('zerochopassword'));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'zerochopassword',
+    cookie: {
+        httpOnly: true,
+    },
+    name: 'connect.sid',
+}));
+app.use('/', (req, res, next) => {  // 미들웨어 확장법
+    if (req.session.id) { // 로그인이 되어 있을경우
+        express.static(__dirname, 'public')(req, res, next)
+    } else { // 되지 않았을 경우 next()로 다음 미들웨어 실행
+        next();
+    }
+});
+    
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(session());
 app.use(multer().array());
 
 app.get('/', (req, res, next) => {
+    req.session.id = 'hello';
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
@@ -32,11 +48,11 @@ app.get('/about', (req, res) => {
     res.send('hello express');
 });
 
-app.use((req, res, next) => { // 일반 미들웨어 - 라우터가 다 무시되고 여기까지 오면 404
+app.use((req, res, next) => { 
     res.status(404).send('404지롱');
 });
 
-app.use((err, req, res, next) => { // 에러 처리 전용 미들웨어 'throw new Error(...)' or 'next(err)' 발생하면
+app.use((err, req, res, next) => { 
     console.error(err);
     res.status(200).send('에러났으니 확인해주세요') 
 })
