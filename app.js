2b4f1bc3f0b5a623ddf63808070786c624fd2aa3
HanChangGyu <ceu0723@gmail.com>
2025-11-11 15:42:18 +0900

    feat: nunjucks기능 추가

--- app.js
+++ app.js
@@ -1,61 +1,46 @@
 const express = require('express');
-const path = require('path');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const multer = reqiuire('multer'); 
-const app = express();
+const dotenv = require('dotenv');
+const path = require('path');
+const nunjucks = require('nunjucks');
 
+dotenv.config();
+const indexRouter = require('./routes'); // 라우터 분리하기
+const userRouter = require('./routes/user');
+
+const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
-app.use(cookieParser('zerochopassword'));
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: 'zerochopassword',
-    cookie: {
-        httpOnly: true,
-    },
-    name: 'connect.sid',
-}));
-app.use('/', (req, res, next) => {  // 미들웨어 확장법
-    if (req.session.id) { // 로그인이 되어 있을경우
-        express.static(__dirname, 'public')(req, res, next)
-    } else { // 되지 않았을 경우 next()로 다음 미들웨어 실행
-        next();
-    }
-});
-    
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(multer().array());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+  name: 'session-cookie',
+}));
 
-app.get('/', (req, res, next) => {
-    req.session.id = 'hello';
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+app.use((req, res, next) => {
+  res.status(404).send('Not Found');
 });
 
-app.post('/', (req, res) => {
-    res.send('hello express!');
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
 });
 
-app.get('/category/Javascript', (req, res) => {
-    res.send(`hello Javascript`);
-});
-
-app.get('/about', (req, res) => {
-    res.send('hello express');
-});
-
-app.use((req, res, next) => { 
-    res.status(404).send('404지롱');
-});
-
-app.use((err, req, res, next) => { 
-    console.error(err);
-    res.status(200).send('에러났으니 확인해주세요') 
-})
 app.listen(app.get('port'), () => {
-    console.log('익스프레스 서버 실행');
+  console.log(app.get('port'), '번 포트에서 대기 중');
 });
